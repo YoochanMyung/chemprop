8e2161ca97a826448f4ddd7eb1f347024279f38f
uqymyung <uqymyung@wiener.hpc.dc.uq.edu.au>
2023-05-16 15:52:30 +1000

    Add split_sizes

--- run_chemprop.py
+++ run_chemprop.py
@@ -2,6 +2,7 @@ import os
 import chemprop
 import sys
 import argparse
+from typing import List, Set, Tuple, Union
 
 def check_categorical(input_pd):
     components = set(input_pd['label'].to_list())
@@ -10,9 +11,17 @@ def check_categorical(input_pd):
     else:
         return True
 
-def run_classification(smi, save_path, split_type, loss_fn, use_rdkit):
-    print("Working on: {}".format(smi))
+def def_split_sizes(train_ratio: float) -> List[str] :
+    train_f = train_ratio
+    val_f = (1- train_f)/2
+    test_f = val_f
 
+    return [train_f, val_f, test_f]
+
+
+def run_classification(smi, save_path, split_type, train_ratio, loss_fn, use_rdkit):
+    print("Working on: {}".format(smi))
+    split_sizes = def_split_sizes(train_ratio)
     if split_type == 'random':
         split_type = 'random_with_repeated_smiles'
     else:
@@ -32,7 +41,8 @@ def run_classification(smi, save_path, split_type, loss_fn, use_rdkit):
     '--loss_function',loss_fn,
     '--save_dir', save_dir,
     '--epochs', '50',
-    '--split_type',split_type,
+    '--split_type', split_type,
+    '--split_sizes', str(split_sizes[0]), str(split_sizes[1]), str(split_sizes[2]),
     '--num_folds','10',
     '--save_smiles_splits',
     '--quiet',
@@ -57,8 +67,9 @@ def run_classification(smi, save_path, split_type, loss_fn, use_rdkit):
         print(smi, e)
 
 
-def run_regression(smi, save_path, split_type, loss_fn, use_rdki):
+def run_regression(smi, save_path, split_type, train_ratio, loss_fn, use_rdki):
     property_name = smi.split('/')[-1].split('.csv')[0]
+    split_sizes = def_split_sizes(train_ratio)
     if split_type == 'random':
         split_type = 'random_with_repeated_smiles'
     else:
@@ -77,6 +88,7 @@ def run_regression(smi, save_path, split_type, loss_fn, use_rdki):
     '--save_dir', save_dir,
     '--epochs', '50',
     '--split_type',split_type,
+    '--split_sizes', str(split_sizes[0]), str(split_sizes[1]), str(split_sizes[2]),
     '--num_folds','10',
     '--save_smiles_splits',
     '--metric', 'r2', # or rmse
@@ -132,6 +144,7 @@ if __name__ == '__main__':
     parser.add_argument("save_path", help="Choose dirpath",type=str)
     parser.add_argument("-run_type", help="Run type between regression and classification",choices=['classification','regression'], default="classification")
     parser.add_argument("-split_type", help="Random or Scaffold",choices=['random','scaffold'], default="random")
+    parser.add_argument("-train_ratio", help="Training ratio", default=0.8, type=float)
     parser.add_argument("-loss_fn", help="[bce,mcc,mse] default is binary cross entropy for classification and mse for regression ",choices=['bce','mcc','mse'], default="default")
     parser.add_argument("-use_rdkit", help="Choose the number of shuffling", action='store_true')
 
@@ -141,12 +154,13 @@ if __name__ == '__main__':
     save_path = args.save_path
     run_type = args.run_type
     split_type = args.split_type
+    train_ratio = args.train_ratio
     loss_fn = args.loss_fn
     use_rdkit = args.use_rdkit
 
     if run_type == 'regression':
-        run_regression(input_csv,save_path,split_type,loss_fn,use_rdkit)
+        run_regression(input_csv,save_path,split_type,train_ratio,loss_fn,use_rdkit)
         run_test(input_csv,save_path,use_rdkit)
     else:
-        run_classification(input_csv,save_path,split_type,loss_fn,use_rdkit)
+        run_classification(input_csv,save_path,split_type,train_ratio,loss_fn,use_rdkit)
         run_test(input_csv,save_path,use_rdkit)
