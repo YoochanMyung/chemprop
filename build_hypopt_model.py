dfda7e7fe6342d6e1388fbc742dadbec433d3459
Yoochan Myung <yuchan.m@gmail.com>
2023-09-27 10:27:34 +1000

    checking wandb and building a model

--- build_hypopt_model.py
+++ build_hypopt_model.py
@@ -4,6 +4,8 @@ import chemprop
 import simplejson as json
 from check_wandb import fetch_result
 import pandas as pd
+import sys
+sys.path.append('/home/ymyung/projects/deeppk/src/chemprop')
 
 def check_categorical(input_csv):
     input_pd = pd.read_csv(input_csv)
@@ -17,16 +19,16 @@ def run(kwargs):
     csv_dir = '/home/ymyung/projects/deeppk/2_ML_running/2_Chemprop/5_using_other_DBs/dataset/admetlab2'
 
     if check_categorical(os.path.join('/home/ymyung/projects/deeppk/2_ML_running/2_Chemprop/5_using_other_DBs/dataset/admetlab2',f'{kwargs.endpoint}_test.csv')):
-        fetch_result({'target': kwargs.endpoint, 'r': False})
+        fetch_result({'target': kwargs.endpoint, 'r': False, 'run_name': kwargs.run_name})
     else:
-        fetch_result({'target': kwargs.endpoint, 'r': True})
+        fetch_result({'target': kwargs.endpoint, 'r': True, 'run_name': kwargs.run_name})
         
     print("Working on: {}".format(kwargs.endpoint))
     print(kwargs)
     save_dir = os.path.join(kwargs.save_path, kwargs.endpoint)
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
-    result_json = json.load(open(os.path.join(f'/home/ymyung/projects/deeppk/2_ML_running/2_Chemprop/5_using_other_DBs/after_hyperopt/admetlab2/{kwargs.endpoint}_best.json'), 'r'))
+    result_json = json.load(open(os.path.join(f'/home/ymyung/projects/deeppk/2_ML_running/2_Chemprop/5_using_other_DBs/after_hyperopt/admetlab2/{kwargs.endpoint}/{kwargs.endpoint}_best.json'), 'r'))
     result_json = {k: str(v) for k, v in result_json.items()}
 
     init_lr = float(result_json['max_lr']) * float(result_json['init_lr_ratio'])
@@ -44,6 +46,7 @@ def run(kwargs):
     ## Model arguments
     '--depth', result_json['depth'],
     '--init_lr', str(init_lr),
+    # '--init_lr_ratio', result_json['init_lr_ratio'],
     '--max_lr', result_json['max_lr'],
     '--final_lr', str(final_lr),
     '--dropout', result_json['dropout'],
@@ -51,16 +54,17 @@ def run(kwargs):
     '--batch_size', result_json['batch_size'],
     '--aggregation', result_json['aggregation'],
     '--hidden_size', result_json['hidden_size'],
-    '--warmup_epochs', result_json['warmup_epochs'],
+    # '--warmup_epochs', '3',
+    '--warmup_epochs', result_json["warmup_epochs"],
     '--ffn_num_layers', result_json['ffn_num_layers'],
     '--ffn_hidden_size', result_json['ffn_hidden_size'],
-    '--aggregation_norm', result_json['aggregation_norm'],
-    '--show_individual_scores',
+    # '--aggregation_norm', '196',
+    '--aggregation_norm', result_json["aggregation_norm"],
     '--metric','mcc',
     '--num_folds','3',
     '--extra_metrics', 'f1','auc','accuracy',
     '--save_preds',
-    '--num_workers','12']
+    '--num_workers','8']
 
     if kwargs.mordred or result_json['mordred'] == 'True':
         arguments = arguments + [
@@ -75,11 +79,12 @@ def run(kwargs):
     # print(arguments)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='ex) python run_chemprop.py aaa.csv ./classification -run_type classification -split_type Random -loss_fn mcc -use_rdkit')
+    parser = argparse.ArgumentParser(description='ex) python build_hypopt_model.py skin_sens . -run_name silvery-dust-1001')
     parser.add_argument("endpoint", help="Choose endpoint name",type=str)
     parser.add_argument("save_path", help="Choose dir path for save",type=str)
     parser.add_argument("-reg", action='store_true', help="")
     parser.add_argument("-mordred", action='store_true', help="Run type between regression and classification")
+    parser.add_argument("-run_name",type=str)
     
     args = parser.parse_args()
     run(args)
--- check_wandb.py
+++ check_wandb.py
@@ -1,56 +1,84 @@
-import wandb, argparse
-import pandas as pd
-import json
-
+import wandb, argparse, json
+import polars as pl
+pl.Config.set_fmt_float("full")
 from tqdm import tqdm
 
 def fetch_result(kwargs):
-	# kwargs = vars(kwargs)
-	target = kwargs['target']
-	reverse = kwargs['r']
-	summary_pd = pd.DataFrame()
+	if isinstance(kwargs, dict):
+		target = kwargs['target']
+		reverse = kwargs['r']
+		run_name = kwargs['run_name']
+	else:
+		target = kwargs.target
+		reverse = kwargs.r
+		run_name = kwargs.run_name
+
+	summary_pd = pl.DataFrame()
+	best_row = dict()
 	api = wandb.Api()	
 	runs = api.runs(f"biosig/ADMETLAB2-hypopt-{target}")
 	# runs = api.runs(f"biosig/ADMETLAB2-hypopt-ames")
-	# print(runs)
-	# import pdb; pdb.set_trace()
-	for run in tqdm(runs):
 
+	for run in tqdm(runs):
 		try:
-			_pd = pd.DataFrame({'mcc_mean': float(run.summary.get('mcc_mean')),\
+			_pd = pl.DataFrame({'mcc_mean': float(run.summary.get('mcc_mean')),\
 								'mcc_std': float(run.summary.get('mcc_std')),
 								'run_name': str(run.name),\
-								'run_id': str(run.id)}, index=[run.name]
-								)
-			_pd = _pd.join(pd.DataFrame(run.config, index=[run.name]))
+								'run_id': str(run.id),\
+								'index' : str(run.name)})
+
+			_pd = pl.concat([_pd, pl.DataFrame(run.config)], how='horizontal')
 		except:
 			pass
-			
-		summary_pd = pd.concat([summary_pd,_pd])			
-	summary_pd.dropna(inplace=True)
-	summary_pd = summary_pd.query('mcc_mean >= 0')
 
-	if not reverse:
-		summary_pd.sort_values(by='mcc_mean', ascending=False, inplace=True)
+		_pd = _pd.select([pl.all().cast(pl.Utf8)])
+		summary_pd = pl.concat([summary_pd,_pd], how='vertical')
+
+	summary_pd = summary_pd.with_columns(pl.col('mcc_mean').cast(pl.Float64))
+	summary_pd_filtered = summary_pd.filter(summary_pd['mcc_mean']>=0)
+	# import pdb;pdb.set_trace()	
+		
+	if not run_name:
+		if not reverse:
+			summary_pd_filtered = summary_pd_filtered.sort('mcc_mean', descending=True)
+		else:
+			summary_pd_filtered = summary_pd_filtered.sort('mcc_mean', descending=False)
+
+		best_row = summary_pd_filtered.row(0, named=True)
+		
+		best_run = api.run(f"biosig/ADMETLAB2-hypopt-{target}/{best_row['run_id']}")
+
 	else:
-		summary_pd.sort_values(by='mcc_mean', ascending=True, inplace=True)
+		run_id = summary_pd_filtered.filter(summary_pd_filtered['run_name'].str.contains(f'{run_name}')).row(0,named=True)['run_id']
+		best_run = api.run(f"biosig/ADMETLAB2-hypopt-{target}/{run_id}")
+		_pd = pl.DataFrame({'mcc_mean': float(best_run.summary.get('mcc_mean')),\
+							'mcc_std': float(best_run.summary.get('mcc_std')),
+							'run_name': str(best_run.name),\
+							'run_id': str(best_run.id),\
+							'index' : str(best_run.name)})
 
-	best_run = api.run(f"biosig/ADMETLAB2-hypopt-{target}/{summary_pd.iloc[0].run_id}")
-	mordred_count = len([each for each in json.load(best_run.file("wandb-metadata.json").download(replace=True))['args'] if 'mordred' in each])
+		_pd = pl.concat([_pd, pl.DataFrame(best_run.config)], how='horizontal')
+		best_row.update(_pd.to_dict(as_series=False))
+		best_row = {key: str(value_list[0]) for key, value_list in best_row.items()}
 
-	if mordred_count > 3:
-		summary_pd.loc[summary_pd.iloc[0].run_name, 'mordred'] = "True"
+	mordred_count = len([each for each in json.load(best_run.file("wandb-metadata.json").download(root=target, replace=True))['args'] if 'mordred' in each])
+
+	if mordred_count > 2:
+		best_row.update({'mordred':'True'})
 	else:
-		summary_pd.loc[summary_pd.iloc[0].run_name, 'mordred'] = "False"
+		best_row.update({'mordred':'False'})
 
-	# print(summary_pd.iloc[0])
-	summary_pd.iloc[0].to_json(f'{target}_best.json')
+	with open(f'./{target}/{target}_best.json', 'w') as fp:
+		json.dump(best_row, fp)
+
+	return best_row
+
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description="A simple argument parser")
 	parser.add_argument("target", type=str, help="A Target name")
 	parser.add_argument("--r", action='store_true')
+	parser.add_argument("-run_name", type=str)
 
 	args = parser.parse_args()
-	# print(args)
-	fetch_result(args)
+	print(fetch_result(args))
